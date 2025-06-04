5ac68faf9f9904353ae3c774e3921605a1bd6363
himorryyyDev <himorryyydev@gmail.com>
2025-06-04 22:29:42 +0300

    contacts fix

--- src/components/layout/ContactComponent/Contact.jsx
+++ src/components/layout/ContactComponent/Contact.jsx
@@ -1,18 +1,19 @@
 import styles from './Contact.module.css'
+import contacts from './Contacts'
 
 const Contact = () => {
 	return (
 		<>
 			<div className={styles.container}>
 				<div className={styles.contact__container}>
-					<a href="tel: +79220229482">
-						<img src="/images/phone.svg" alt="call" />
-						<p>{'+7 (922) 022-94-82'}</p>
-					</a>
-					<a href="mailto: janis62@yahoo.com">
-						<img src="/images/mail.svg" alt="mail" />
-						<p>{'janis62@yahoo.com'}</p>
-					</a>
+					{contacts.map((contact) => {
+						return(
+							<a href={contact.link} className={styles.contact__item}>
+								<img src={contact.image} alt={contact.alt} />
+								<p>{contact.title}</p>
+							</a>
+						)
+					})}
 				</div>
 				<button className={styles.call}>Заказать звонок</button>
 			</div>
---
+++ src/components/layout/ContactComponent/Contacts.js
@@ -0,0 +1,16 @@
+const contacts = [
+  {
+    title: '+7 (922) 022-94-82',
+    link: 'tel: +79220229482',
+    image: '/images/phone.svg',
+    alt: 'call'
+  },
+  {
+    title: 'janis62@yahoo.com',
+    link: 'mailto: janis62@yahoo.com',
+    image: '/images/mail.svg',
+    alt: 'mail'
+  },
+]
+
+export default contacts
