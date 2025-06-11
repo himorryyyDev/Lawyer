f0bd0af7bfaf2cb1292fa9c26d00e0c3cb6a4127
himorryyyDev <himorryyydev@gmail.com>
2025-06-11 20:27:56 +0300

    slider

--- src/components/layout/PersonalSection/Personal.jsx
+++ src/components/layout/PersonalSection/Personal.jsx
@@ -13,13 +13,23 @@ const Personal = () => {
         <div className={styles.person__container}>
           {personList.map((person) => {
             return (
-              <div className={styles.person__item}>
+              <div className={styles.person__item} key={person.name}>
                 <div className={styles.item__preview}>
                   <img className={styles.preview__image} src={person.image} alt="" />
                   <h1 className={styles.person__name}>{person.name}</h1>
                   <p className={styles.person__position}>{person.position}</p>
                 </div>
-                <div className={styles.item__info}></div>
+                <div className={styles.item__info}>
+                  <div className={styles.info__container}>
+                    <div className={styles.person__main}>
+                      <h1 className={styles.person__name}>{person.name}</h1>
+                      <p className={styles.person__position}>{person.position}</p>
+                    </div>
+                    <p className={styles.person__description}>{person.description}</p>
+                    <p className={styles.person__seniority}>{person.seniority}</p>
+                  </div>
+                  <div className={styles.inner}></div>
+                </div>
               </div>
             )
           })}
