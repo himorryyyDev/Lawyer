e88b5d7523626c385f36ccbeaea37d6e074e965e
himorryyyDev <himorryyydev@gmail.com>
2025-06-20 14:34:47 +0300

    review fix

--- src/components/layout/ReviewsSection/ReviewSlider Component/ReviewSlider.jsx
+++ src/components/layout/ReviewsSection/ReviewSlider Component/ReviewSlider.jsx
@@ -1,16 +1,23 @@
 import reviewList from "./ReviewList"
 import styles from './ReviewSlider.module.css'
+import defaultAvatar from '/images/default-user.jpg'
 
 const ReviewSlider = () => {
+  const handeImageError = (e) => {
+    e.target.src = defaultAvatar
+  }
+
   return (
     <>
       <section className={styles.review__slider}>
-        {reviewList.map((review) => {
+        {reviewList.slice(0, 2).map((review) => {
           return (
             <div key={review.name} className={styles.slider__item}>
               <div className={styles.item__info}>
                 <div className={styles.info__person}>
-                  <img className={styles.person__image} src={review.image} alt="" />
+                  <div className={styles.person__image}>
+                    <img className={styles.image__item} src={review.image || defaultAvatar} onError={handeImageError} alt="" />
+                  </div>
                   <div className={styles.person__info}>
                     <p className={styles.info__company}>{review.company}</p>
                     <h1 className={styles.info__name}>{review.name}</h1>
